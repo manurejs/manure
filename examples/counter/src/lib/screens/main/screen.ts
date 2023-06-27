b06c7085f7027df90a147ecabd2efa84bc8fdca3
chientrm <chientrm@gmail.com>
2023-06-27 08:10:38 +0000

    dead code + format

--- examples/counter/src/lib/screens/main/screen.ts
+++ examples/counter/src/lib/screens/main/screen.ts
@@ -3,7 +3,11 @@ import { Column, StatelessWidget, Text, Widget } from 'manure';
 class MainScreen extends StatelessWidget {
 	build(): Widget {
 		return new Column({
-			children: [new Text('Hello World!'), new Text('GG')],
+			children: [
+				new Text('Hello World!'),
+				new Text('Welcome to ManureJS'),
+				new Text("I'm chientrm"),
+			],
 		});
 	}
 }
--- packages/manure/index.ts
+++ packages/manure/index.ts
@@ -1,7 +1,6 @@
-import { App } from "./src/components/app";
 import { Column } from "./src/components/column";
 import { Text } from "./src/components/text";
 import { post, render } from "./src/framework";
 import { StatelessWidget, Widget } from "./src/widget";
 
-export { App, StatelessWidget, Text, Widget, post, render, Column };
+export { Column, StatelessWidget, Text, Widget, post, render };
